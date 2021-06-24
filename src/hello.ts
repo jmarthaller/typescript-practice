ec4efac0f01853aeeb5765c0fc1351b9a7ee8260
Jacob Marthaller <marthaller.jr@gmail.com>
2021-06-24 10:44:59 -0700

    configure migrations pre postgresql setup

--- src/hello.ts
+++ src/hello.ts
@@ -9,16 +9,10 @@
 import {MikroORM } from "@mikro-orm/core";
 import { __prod__ } from "./constants";
 import { Post } from "./entities/Post";
+import microConfig from './mikro-orm.config'
 
 const main = async () => {
-    const orm = await MikroORM.init({
-        entities: [Post],
-        dbName: 'portfolio',
-        user: '',
-        password: '',
-        type: 'postgresql',
-        debug: !__prod__,
-    });
+    const orm = await MikroORM.init(microConfig);
 
     const post = orm.em.create(Post, { title: 'my posts'});
     await orm.em.persistAndFlush(post);
