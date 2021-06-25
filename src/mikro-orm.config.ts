aca07e349b4637c2d20d059d26f7b75c6b19cffc
Jacob Marthaller <marthaller.jr@gmail.com>
2021-06-25 11:29:10 -0700

    Initial migration

--- src/mikro-orm.config.ts
+++ src/mikro-orm.config.ts
@@ -10,9 +10,11 @@ export default {
         pattern: /^[\w-]+\d+\.[tj]s$/,
     },
     entities: [Post],
-    dbName: 'portfolio',
+    dbName: 'lireddit',
     type: 'postgresql',
     debug: !__prod__,
 } as Parameters<typeof MikroORM.init>[0];
+
+
 
 
