a02532d08a0f4bc9fac143eca6d8c178437eadd6
Jacob Marthaller <marthaller.jr@gmail.com>
2021-06-29 11:30:06 -0700

    Migrate user into table

---
+++ src/resolvers/user.ts
@@ -0,0 +1,9 @@
+import { Resolver, Query, Mutation } from 'type-graphql';
+
+@Resolver()
+export class UserResolver {
+    @Mutation(() => String)
+    register() {
+        return 'hello world'
+    }
+}
