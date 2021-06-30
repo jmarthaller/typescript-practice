7d660c858f44c2e9702b924cda7d92d3cfa1d760
Jacob Marthaller <marthaller.jr@gmail.com>
2021-06-30 11:44:04 -0700

    update working backend to run sessions with cookies

--- src/resolvers/hello.ts
+++ src/resolvers/hello.ts
@@ -6,4 +6,6 @@ export class HelloResolver {
     hello() {
         return 'hello world'
     }
+
+    
 }
--- src/resolvers/user.ts
+++ src/resolvers/user.ts
@@ -1,6 +1,6 @@
 import { User } from '../entities/User';
 import { MyContext } from 'src/types';
-import { Resolver, Mutation, Arg, InputType, Field, Ctx, ObjectType } from 'type-graphql';
+import { Resolver, Mutation, Arg, InputType, Field, Ctx, ObjectType, Query } from 'type-graphql';
 import argon2 from 'argon2';
 
 @InputType()
@@ -30,10 +30,25 @@ class UserResponse {
 
 @Resolver()
 export class UserResolver {
+
+    @Query(() => User, {nullable: true})
+    async me(
+        @Ctx() { req, em }: MyContext
+    ) {
+        if (!req.session.userId) {
+            return null
+        }
+
+        const user = await em.findOne(User, { id: req.session.userId });
+        return user;
+    }
+
+
+
     @Mutation(() => UserResponse)
     async register(
         @Arg('options') options: UsernamePasswordInput,
-        @Ctx() { em }: MyContext
+        @Ctx() { em, req }: MyContext
     ): Promise<UserResponse>
     {
         if (options.username.length <= 2) {
@@ -67,7 +82,7 @@ export class UserResolver {
                 }
             }
         }
-        
+        req.session.userId = user.id;
         return { user };
     }
 
