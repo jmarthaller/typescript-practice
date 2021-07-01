18b6502a35691a060f881eb3d3c63880375c14b8
Jacob Marthaller <marthaller.jr@gmail.com>
2021-07-01 07:06:42 -0700

    Build Register Query with Knex

--- src/resolvers/user.ts
+++ src/resolvers/user.ts
@@ -2,6 +2,7 @@ import { User } from '../entities/User';
 import { MyContext } from 'src/types';
 import { Resolver, Mutation, Arg, InputType, Field, Ctx, ObjectType, Query } from 'type-graphql';
 import argon2 from 'argon2';
+import { EntityManager } from '@mikro-orm/postgresql'
 
 @InputType()
 class UsernamePasswordInput {
@@ -69,9 +70,18 @@ export class UserResolver {
         }
     }
         const hashedPassword = await argon2.hash(options.password);
-        const user = em.create(User, { username: options.username, password: hashedPassword });
+        let user;
+        // const user = em.create(User, { username: options.username, password: hashedPassword });
         try {
-            await em.persistAndFlush(user);
+            const result = await (em as EntityManager).createQueryBuilder(User).getKnexQuery().insert(
+                { 
+                    username: options.username, 
+                    password: hashedPassword, 
+                    created_at: new Date(),
+                    updated_at: new Date(),
+                }).returning("*");
+                user = result[0];
+            // await em.persistAndFlush(user);
         } catch (err) {
             if (err.code === "23305" || err.detail.includes("already exists")) {
                 return {
