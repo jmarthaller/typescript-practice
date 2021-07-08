4d0f94a61461aeba335a478dc258ef0c21a69dc0
Jacob Marthaller <marthaller.jr@gmail.com>
2021-07-08 07:01:22 -0700

    Delete cruft from backend

--- src/types.ts
+++ src/types.ts
@@ -1,10 +1,9 @@
 import { Request, Response } from "express";
 import { Redis } from "ioredis";
 
-
 export type MyContext = {
-    req: Request & { session: any };
-    // ALTERNATIVE TYPES CAUSING UNPREDICATABLE BUG req: Request & { session: Express.Session };
-    redis: Redis;
-    res: Response;
-}
+  req: Request & { session: any };
+  // ALTERNATIVE TYPES CAUSING UNPREDICATABLE BUG req: Request & { session: Express.Session };
+  redis: Redis;
+  res: Response;
+};
