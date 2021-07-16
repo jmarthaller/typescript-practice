c6f5fc58dd98238e13e06acaf1bb900405567271
Jacob Marthaller <marthaller.jr@gmail.com>
2021-07-16 08:04:57 -0700

    Make comment on TypeScript types.ts more verbose for production environment

--- src/types.ts
+++ src/types.ts
@@ -3,7 +3,8 @@ import { Redis } from "ioredis";
 
 export type MyContext = {
   req: Request & { session: any };
-  // ALTERNATIVE TYPES CAUSING UNPREDICATABLE BUG req: Request & { session: Express.Session };
+  // Types Causing Unpredictable bugs 
+  // Fix before moving to production req: Request & { session: Express.Session };
   redis: Redis;
   res: Response;
 };
