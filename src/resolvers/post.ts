b24b28522d0697121d72fae28f9157d7b0222c24
Jacob Marthaller <marthaller.jr@gmail.com>
2021-07-24 10:42:50 -0700

    Add ability to change vote

--- src/resolvers/post.ts
+++ src/resolvers/post.ts
@@ -16,7 +16,8 @@ import {
 } from "type-graphql";
 import { Post } from "../entities/Post";
 import { getConnection } from "typeorm";
-// import { Upvote } from "../entities/Upvote";
+// import { Upvote } from "src/entities/Upvote";
+import { Upvote } from "../entities/Upvote";
 
 @InputType()
 class PostInput {
@@ -53,22 +54,52 @@ export class PostResolver {
     const isUpvote = value !== -1; 
     const realValue = isUpvote ? 1 : -1;
     const { userId } = req.session;
-    // await Upvote.insert({
-    //   userId,
-    //   postId, 
-    //   value: realValue,
-    // });
-    await getConnection().query(`
-      START TRANSACTION;
-      insert into upvote ("userId", "postId", value)
-      values (${userId},${postId},${realValue});
-      update post
-      set points = points + ${realValue}
-      where id = ${postId};
-      COMMIT;
-    `);
+    const upvote = await Upvote.findOne({where: { postId, userId }}) 
 
-    return true;
+    if (upvote && upvote.value !== realValue) {
+      await getConnection().transaction(async (tm) => {
+        await tm.query(`
+          update upvote 
+          set value = $1
+          where "postId" = $2 and "userId" = $3
+        `, [realValue,postId,userId]);
+
+
+        await tm.query(`
+          update post
+          set points = points + $1
+          where id = $2;
+        `, [2* realValue, postId]);
+        })
+        
+
+
+    } else if (!upvote) {
+      await getConnection().transaction(async tm => {
+        await tm.query(`
+          insert into upvote ("userId", "postId", value)
+          values ($1, $2, $3);
+        `, [userId,postId,realValue]);
+
+        await tm.query(`
+          update post
+          set points = points + $1
+          where id = $2;
+        `, [realValue,postId]);
+      });
+    }
+
+    // await getConnection().query(`
+    //   START TRANSACTION;
+    //   insert into upvote ("userId", "postId", value)
+    //   values (${userId},${postId},${realValue});
+    //   update post
+    //   set points = points + ${realValue}
+    //   where id = ${postId};
+    //   COMMIT;
+    // `);
+
+    // return true;
   }
 
   @Query(() => PaginatedPosts)
