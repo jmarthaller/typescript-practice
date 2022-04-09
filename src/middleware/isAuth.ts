1abba099f81acaf54359a58620b451f01b45f2ab
Jacob Marthaller <marthaller.jr@gmail.com>
2022-04-09 22:34:00 +0300

    format isAuth

--- src/middleware/isAuth.ts
+++ src/middleware/isAuth.ts
@@ -5,6 +5,5 @@ export const isAuth: MiddlewareFn<MyContext> = ({ context }, next) => {
   if (!context.req.session.userId) {
     throw new Error("you are not logged in!");
   }
-
   return next();
 };
