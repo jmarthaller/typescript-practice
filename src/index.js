8fd3ee7f25dd3496733d3599b54bbd324733bd59
Jacob Marthaller <marthaller.jr@gmail.com>
2022-04-27 21:00:00 +0300

    format index.js

--- src/index.js
+++ src/index.js
@@ -1,14 +1,14 @@
-import {MikroORM } from "@mikro-orm/core";
+import { MikroORM } from "@mikro-orm/core";
 import { __prod__ } from "./constants";
 import { Post } from "./entities/Post";
-import microConfig from './mikro-orm.config'
+import microConfig from "./mikro-orm.config";
 
 const main = async () => {
-    const orm = await MikroORM.init(microConfig);
-    orm.getMigrator().up();
-    const post = orm.em.create(Post, { title: 'my posts'});
-    await orm.em.persistAndFlush(post);
-}
-main()
+  const orm = await MikroORM.init(microConfig);
+  orm.getMigrator().up();
+  const post = orm.em.create(Post, { title: "my posts" });
+  await orm.em.persistAndFlush(post);
+};
+main();
 
 // test test
