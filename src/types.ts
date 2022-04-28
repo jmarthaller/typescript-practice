e2a53c4a3fd29ada94a117813e6cc517b9c602fd
Jacob Marthaller <marthaller.jr@gmail.com>
2022-04-28 22:00:00 +0300

    delete cruft in types.ts

--- src/types.ts
+++ src/types.ts
@@ -3,8 +3,6 @@ import { Redis } from "ioredis";
 
 export type MyContext = {
   req: Request & { session: any };
-  // Types Causing Unpredictable bugs 
-  // Fix before moving to production req: Request & { session: Express.Session };
   redis: Redis;
   res: Response;
 };
